695c788bb6569adf0c5faededf2a1e8e6dc9b00f
Avichai Alafi <alafi.avichai@bankleumi.co.il>
2023-10-03 16:54:07 +0300

    building game cards

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'c7b18323a47d40c394ea5b019646b1f5'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "698c6c6aed1549e68fb93089cdbf5086",
+  },
+});
