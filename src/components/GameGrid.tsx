08b5e301631cc564517335542d9318f4dea84763
Avichai Alafi <alafi.avichai@bankleumi.co.il>
2023-10-06 06:21:50 +0300

    refactor: gameQuery

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,20 +3,16 @@ import useGames, { Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import { Genre } from "../hooks/useGenres";
+import { GameQuery } from "../App";
 
 const skeletonArr = [1, 2, 3, 4, 5, 6];
 
 interface Props {
-  selectedGenre: Genre | null;
-  selectedPlatform: Platform | null;
+  gameQuery: GameQuery;
 }
 
-const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
-  const {
-    data: games,
-    error,
-    isLoading,
-  } = useGames(selectedGenre, selectedPlatform);
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data: games, error, isLoading } = useGames(gameQuery);
 
   return (
     <>
