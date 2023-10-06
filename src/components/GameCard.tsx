b98d868b306b982e23f237457e8853e05be214ab
Avichai Alafi <alafi.avichai@bankleumi.co.il>
2023-10-06 07:03:14 +0300

    clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -15,8 +15,7 @@ const GameCard = ({ game }: Props) => {
       <Card>
         <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
-          <Heading fontSize="2xl">{game.name}</Heading>
-          <HStack justifyContent="space-between">
+          <HStack marginBottom={3} justifyContent="space-between">
             <PlatformItemList
               platforms={game.parent_platforms.map(
                 (platform) => platform.platform
@@ -24,6 +23,7 @@ const GameCard = ({ game }: Props) => {
             />
             <CriticScore score={game.metacritic} />
           </HStack>
+          <Heading fontSize="2xl">{game.name}</Heading>
         </CardBody>
       </Card>
     </GameCardContainer>
